539102a4103ffec72b5db3ec0b2b831d812550ac
DiegoSalazar <d.salazararp@gmail.com>
2023-10-22 15:25:59 -0300

    Se agrega func de hijos en notas, carga en sidebar

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -8,16 +8,16 @@ import Navigation from "./_components/navigation";
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated, isLoading } = useConvexAuth();
 
-  if (!isAuthenticated) {
-    return redirect("/");
-  }
-
   if (isLoading) {
     return (
       <div className="h-full flex items-center justify-center">
         <Spinner size="lg" />
       </div>
     );
+  }
+
+  if (!isAuthenticated) {
+    return redirect("/");
   }
 
   return (
